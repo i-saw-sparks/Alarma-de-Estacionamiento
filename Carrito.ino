e3345cec48ab088147f913082104d747c5044117
i-saw-sparks <andresmolinaceti@gmail.com>
2018-10-27 18:54:48 -0500

    V1.0

--- Carrito.ino
+++ Carrito.ino
@@ -2,7 +2,7 @@
 #include <Adafruit_ST7735.h>
 #include <Adafruit_GFX.h>
 #include <TimerOne.h>
-
+#include <Keypad.h>
 
 #define Trig 2
 #define Echo 3
@@ -10,12 +10,27 @@
 #define TFT_DC 9
 #define TFT_RST 8
 
+char hexaKeys[4][4]=
+{
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'},
+};
+
+byte rowPins[4] = {47, 45, 43, 41};
+byte colPins[4] = {39, 37, 35, 33};
+
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+Keypad Teclado = Keypad(makeKeymap(hexaKeys), rowPins, colPins, 4, 4);
 
 const float VelSon = 34000.0;
 
 int Contador=0;
 float Distancia=26;
+bool Status=false, Rev=false;
+char Tecla='P';
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,16 +39,38 @@ void setup() {
   pinMode(Echo, INPUT);
 
   TimerOne Timer1;
-  Timer1.initialize(100000);
-  Timer1.attachInterrupt(Sonido);
-  interrupts();
   tft.initR(INITR_BLACKTAB);
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(2);
-  
+  tft.fillScreen(ST77XX_BLACK);
 }
 
 void loop() {
+  tft.fillScreen(ST77XX_BLACK);
+  
+  
+    Tecla=Teclado.getKey();
+    Serial.println(Tecla);
+ 
+
+  if(Tecla=='*')
+    Status=true;
+
+  while(Rev==false&&Status==true){
+    tft.setCursor(10,10);
+    tft.print("D");
+    Tecla=Teclado.getKey();
+    if(Tecla=='1'){
+      Rev=true;
+      Timer1.initialize(100000);
+      Timer1.attachInterrupt(Sonido);
+      interrupts();
+    }
+    if(Tecla=='#')
+      Status=false;
+  }
+  
+  while(Rev==true&&Status==true){
   iniciarTrigger();
   unsigned long tiempo = pulseIn(Echo, HIGH);
   Distancia = tiempo * 0.000001 * VelSon / 2.0;
@@ -61,9 +98,9 @@ void loop() {
           tft.setTextColor(ST77XX_WHITE);
         }
      }
-  }
-  
-  
+  } 
+  tft.setCursor(10,10);
+    tft.print("R");
   if(Distancia>2000){
     tft.setCursor(50,75);
     tft.println("OK");
@@ -73,9 +110,18 @@ void loop() {
     tft.print(Distancia);
     tft.println("cm");
   }
-  
-  
+  Tecla=Teclado.getKey();
+    if(Tecla=='2'){
+      Rev=false;
+      Timer1.stop();
+    }
+    if(Tecla=='#'){
+      Status=false;
+      Rev=false;
+      Timer1.stop();
+    }
   delay(500);
+  }
 }
 
 void iniciarTrigger()
